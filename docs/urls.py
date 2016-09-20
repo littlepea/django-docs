e4c7d94f91a6ed579ea1a47f5bb25dd98bc52448
Eugene „Stilgar“ Gilewski <stilgar.dev@gilevski.name>
2016-09-20 18:31:13 +0200

    Fix RedirectView.permanent again
    Reapply https://github.com/littlepea/django-docs/pull/2 probably accidentally overwritten in https://github.com/littlepea/django-docs/commit/d9af1928fc8375f5e09ec0b1d874b429e4ba76ac

--- docs/urls.py
+++ docs/urls.py
@@ -3,6 +3,6 @@ from docs.views import DocsRootView, serve_docs
 
 
 urlpatterns = [
-    url(r'^$', DocsRootView.as_view(), name='docs_root'),
+    url(r'^$', DocsRootView.as_view(permanent=True), name='docs_root'),
     url(r'^(?P<path>.*)$', serve_docs, name='docs_files'),
 ]
