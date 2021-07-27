d3464d50ea9ae6f53c28db48a755dbc035980adc
François Poulain <fpoulain@metrodore.fr>
2021-07-27 16:26:55 +0200

    Replace deprecated django.conf.urls.url by django.conf.urls.re_path

--- docs/urls.py
+++ docs/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 from docs.views import DocsRootView, serve_docs, DOCS_DIRHTML
 
 urlpatterns = []
 
 if not DOCS_DIRHTML:
     urlpatterns += [
-        url(r'^$', DocsRootView.as_view(permanent=True), name='docs_root'),
+        re_path(r'^$', DocsRootView.as_view(permanent=True), name='docs_root'),
     ]
 
 urlpatterns += [
-    url(r"^(?P<path>.*)$", serve_docs, name="docs_files"),
+    re_path(r"^(?P<path>.*)$", serve_docs, name="docs_files"),
 ]
